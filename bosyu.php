daed05a7b2a60b0311c3892744741b9d8afa2223
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-11-04 09:47:03 +0900

    bosyu.php追加
    求人用ページを追加

---
+++ bosyu.php
@@ -0,0 +1,98 @@
+<?php
+//アクセス
+//引数
+// strcode 店舗番号 [推奨] ない場合は1になる
+// 最小引数 ?strcode=1
+
+require_once("php/view.function.php");
+require_once("php/html.function.php");
+
+//ファイル名
+$me="bosyu.php";
+
+//店舗番号確定
+if($_GET["strcode"] && preg_match("/^[0-9]+$/",$_GET["strcode"])){
+ $strcode=$_GET["strcode"];
+}
+else{
+ $strcode=1;
+}
+
+
+//タイトル
+$title="会社概要";
+
+//ページ説明文
+$description="";
+htmlHeader($title,$description);
+
+//print_r($item);
+?>
+
+  <div id="wrapper">
+   <div class="col1">
+<?php
+echo htmlNaviBar();
+?>
+   </div><!--div class="col1"-->
+
+   <div class="col1">
+    <h1>求人募集</h1>
+    <p>
+     現在、以下の求人募集を行っております。キタムラで一緒にお仕事できる方、ぜひご連絡ください。
+    </p>
+    <table id="ItemData">
+     <colgroup>
+      <col class="nen">
+     </colgroup>
+     <colgroup>
+      <col class="naiyo">
+     </colgroup>
+     <tbody>
+      <tr>
+       <td style="text-align:center;">部門</td>
+       <td>レジおよび品出し</td>
+      </tr>
+      <tr>
+       <td style="text-align:center;">時給</td>
+       <td>1000円（高校生950円）</td>
+      </tr>
+      <tr>
+       <td style="text-align:center;">時間</td>
+       <td>18:00-22:00</td>
+      </tr>
+      <tr>
+       <td style="text-align:center;">曜日</td>
+       <td>週3-5日勤務できる方(土日いずれか勤務)</td>
+      </tr>
+      <tr>
+       <td style="text-align:center;">詳細</td>
+       <td>試用期間(2週間)有り。制服一部貸与。電話連絡の上、履歴書（写真添付）をご持参ください。</td>
+      </tr>
+     </tbody>
+    </table>
+   </div><!--div class="col1"-->
+
+<?php
+htmlSNSButton();
+?>
+   <div class="clr"></div>
+
+
+
+   <div id="footer">
+<?php
+htmlFooter();
+?>
+   </div><!--div id="footer"-->
+
+  </div><!--div id="wrapper"-->
+ </body>
+<script>
+$(function(){
+});
+</script>
+</html>
+
+
+
