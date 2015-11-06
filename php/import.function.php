c9312555cd6bf4cec57f89b451364e57c7ac48f6
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-11-06 16:59:57 +0900

    ギフト・早期ご予約インポート時の既存データ扱いを追加
    	modified:   php/import.function.php

--- php/import.function.php
+++ php/import.function.php
@@ -105,6 +105,24 @@ function impFile2DB($tablename,$filename){
        echo "既存データ削除完了<br>";
       }
       
+      //ギフトは1行目の日付以降を一括削除
+      if($sql[0]["col"]["saletype"]==8){
+       $db->from=TABLE_PREFIX.JANSALE;
+       $db->where =" saleday >='".$sql[0]["col"]["saleday"]."'";
+       $db->where.=" and saletype=8";
+       $db->delete();
+       echo "既存データ削除完了<br>";
+      }
+      
+      //早期ご予約は1行目の日付以降を一括削除
+      if($sql[0]["col"]["saletype"]==9){
+       $db->from=TABLE_PREFIX.JANSALE;
+       $db->where =" saleday >='".$sql[0]["col"]["saleday"]."'";
+       $db->where.=" and saletype=9";
+       $db->delete();
+       echo "既存データ削除完了<br>";
+      }
+      
       //店舗イベントは一括削除
       if($sql[0]["col"]["saletype"]==7){
        $db->from=TABLE_PREFIX.JANSALE;
