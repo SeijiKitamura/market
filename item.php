617a82dd4ecf9290ca170823c316686023ae3c0c
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-11-15 14:36:50 +0900

    item.php 手直し
    	modified:   item.php

--- item.php
+++ item.php
@@ -12,9 +12,6 @@
 require_once("php/view.function.php");
 require_once("php/html.function.php");
 
-//ファイル名
-$me="item.php";
-
 //店舗番号確定
 if($_GET["strcode"] && preg_match("/^[0-9]+$/",$_GET["strcode"])){
  $strcode=$_GET["strcode"];
