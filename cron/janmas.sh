23ddfd002fc9069fd255c73c9bb494b36dfde74e
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-12-28 09:25:54 +0900

    単品マスタ更新にメーカーマスタを反映するように変更
    	modified:   cron/janmas.sh

--- cron/janmas.sh
+++ cron/janmas.sh
@@ -3,14 +3,18 @@
 #変数セット
 DATADIR=data
 FNAME=janmas.csv
+LOGFILE=logfile.txt
 
 cd $(dirname $0)
 cd ../${DATADIR}
 #echo `pwd`
 
+echo `date +%Y-%m-%d_%H:%M:%S` start>>$LOGFILE
+d=`date +%Y%m%d%H%M%S`
+
 #ファイル存在チェック
 if [ ! -e ${FNAME} ]; then
- echo "${FNAME}がありません"
+ echo "${FNAME}がありません">>$LOGFILE
  exit 1
 fi
 
@@ -21,12 +25,18 @@ fi
 
 #ファイル読み込み
 while read LINE; do
- echo $LINE|awk -F',' '{printf("DELETE FROM ultra_janmas where strcode=%d and jcode=\047%d\047;"),$1,$3;print "";
+ echo $LINE|awk -F',' '{printf("DELETE FROM ultra_janmas where strcode=%s and jcode=\x27%s\x27;"),$1,$3;print "";
                         printf("INSERT INTO ultra_janmas(strcode,clscode,jcode,sname,maker,tani,stdprice,price,comment,firstsale,lastsale) VALUES(%s,%s,\x27%s\x27,\x27%s\x27,\x27%s\x27,\x27%s\x27,%s,%s,\x27%s\x27,\x27%s\x27,\x27%s\x27);",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);print ""}'>>sql.txt 
 done < ${FNAME}
 
 echo "update ultra_janmas set firstsale='1970/1/1' where firstsale='1900/1/1';">>sql.txt
 echo "update ultra_janmas set lastsale ='1970/1/1' where lastsale='1900/1/1'; ">>sql.txt
 
+echo "update ultra_janmas set maker=ultra_makermas.maker from ultra_makermas where ultra_makermas.jcode=substr(ultra_janmas.jcode,1,6) and ultra_janmas.maker='';">>sql.txt
+echo "update ultra_janmas set maker=ultra_makermas.maker from ultra_makermas where ultra_makermas.jcode=substr(ultra_janmas.jcode,1,7) and ultra_janmas.maker='';">>sql.txt
+echo "update ultra_janmas set maker=ultra_makermas.maker from ultra_makermas where ultra_makermas.jcode=substr(ultra_janmas.jcode,1,9) and ultra_janmas.maker='';">>sql.txt
 psql -f sql.txt >/dev/null
 
+#cp sql.txt ${d}sql.txt
+
+echo `date +%Y-%m-%d_%H:%M:%S` end>>$LOGFILE
